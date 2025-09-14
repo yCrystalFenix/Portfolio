0f354d5bcc2d4302a41f249dc907c30ae3a75840
yCrystalFenix <105086743+yCrystalFenix@users.noreply.github.com>
2025-09-14 11:50:58 -0300

    Update RobloxStats.tsx

--- src/components/RobloxStats.tsx
+++ src/components/RobloxStats.tsx
@@ -8,7 +8,7 @@ import { robloxApi, RobloxGameConfig, RobloxGameStats, RobloxStatsTotal } from "
 
 // Add your games here - easily modular!
 const GAMES_CONFIG: RobloxGameConfig[] = [
-  { name: "Jump Rope", placeId: 7815946137 },
+  { name: "Jump Rope", placeId: 94371891827792 },
   { name: "Find the Monster", placeId: 111323207426362 },
   { name: "Climb A Wall", placeId: 16989186790 },
 ];
